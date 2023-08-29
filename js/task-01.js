c54bc3ef3b8c6c1756736c191e96706bd5c69177
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2023-08-29 14:11:47 +0300

    Task1
    Виконання Task1

---
+++ js/task-01.js
@@ -0,0 +1,29 @@
+//1
+// Отримуємо елемент <ul> за його ідентифікатором
+const categoriesList = document.getElementById('categories');
+
+// Отримуємо всі елементи <li> з класом 'item' всередині <ul>
+const itemElements = categoriesList.querySelectorAll('li.item');
+
+// Підраховуємо кількість знайдених елементів
+const itemCount = itemElements.length;
+
+// Виводимо кількість категорій в консоль
+console.log(`Кількість категорій: ${itemCount}`);
+
+
+//2
+// Проходимось по кожному елементу <li> з класом 'item'
+itemElements.forEach(item => {
+    // Знаходимо елемент <h2> всередині поточного <li>
+    const titleElement = item.querySelector('h2');
+    
+    // Знаходимо всі дочірні елементи <li> всередині поточного <li>
+    const subItemElements = item.querySelectorAll('ul > li');
+
+    // Підраховуємо кількість знайдених підкатегорій
+    const subItemCount = subItemElements.length;
+
+    // Виводимо текст заголовка і кількість підкатегорій в консоль
+    console.log(`Категорія: ${titleElement.textContent}, Елементи: ${subItemCount}`);
+});

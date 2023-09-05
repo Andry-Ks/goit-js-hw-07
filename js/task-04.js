f8cae98296b8de99ead26dbefdbd27e356587151
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2023-09-06 01:35:59 +0300

    t4

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxes = document.querySelector("#boxes");
+  const width = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = width + i * 10 + "px";
+    box.style.height = width + i * 10 + "px";
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxes.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxes = document.querySelector("#boxes");
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.firstChild);
+  }
+}
+
+const controls = document.querySelector("#controls");
+const createButton = controls.querySelector("button[data-create]");
+const destroyButton = controls.querySelector("button[data-destroy]");
+
+createButton.addEventListener("click", () => {
+  const input = controls.querySelector("input");
+  const amount = parseInt(input.value);
+
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});

72620634fd1c1aa2a10ff6cc0c383de0832273ef
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2023-09-21 14:48:42 +0300

    formatter

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 //1
 // Отримуємо елемент <ul> за його ідентифікатором
-const categoriesList = document.getElementById('categories');
+const categoriesList = document.getElementById("categories");
 
 // Отримуємо всі елементи <li> з класом 'item' всередині <ul>
-const itemElements = categoriesList.querySelectorAll('li.item');
+const itemElements = categoriesList.querySelectorAll("li.item");
 
 // Підраховуємо кількість знайдених елементів
 const itemCount = itemElements.length;
@@ -11,19 +11,20 @@ const itemCount = itemElements.length;
 // Виводимо кількість категорій в консоль
 console.log(`Кількість категорій: ${itemCount}`);
 
-
 //2
 // Проходимось по кожному елементу <li> з класом 'item'
-itemElements.forEach(item => {
-    // Знаходимо елемент <h2> всередині поточного <li>
-    const titleElement = item.querySelector('h2');
-    
-    // Знаходимо всі дочірні елементи <li> всередині поточного <li>
-    const subItemElements = item.querySelectorAll('ul > li');
+itemElements.forEach((item) => {
+  // Знаходимо елемент <h2> всередині поточного <li>
+  const titleElement = item.querySelector("h2");
 
-    // Підраховуємо кількість знайдених підкатегорій
-    const subItemCount = subItemElements.length;
+  // Знаходимо всі дочірні елементи <li> всередині поточного <li>
+  const subItemElements = item.querySelectorAll("ul > li");
 
-    // Виводимо текст заголовка і кількість підкатегорій в консоль
-    console.log(`Категорія: ${titleElement.textContent}, Елементи: ${subItemCount}`);
+  // Підраховуємо кількість знайдених підкатегорій
+  const subItemCount = subItemElements.length;
+
+  // Виводимо текст заголовка і кількість підкатегорій в консоль
+  console.log(
+    `Категорія: ${titleElement.textContent}, Елементи: ${subItemCount}`
+  );
 });
--- js/task-03.js
+++ js/task-03.js
@@ -1,30 +1,28 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-
 // Отримуємо посилання на елемент ul.gallery
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 // Створюємо рядок для зберігання HTML-коду галереї
-let galleryHTML = '';
+let galleryHTML = "";
 
 // Проходимося по масиву зображень та створюємо елементи <li> з <img> та додаємо класи
-images.forEach(image => {
+images.forEach((image) => {
   galleryHTML += `<li class="gallery-item"><img src="${image.url}" alt="${image.alt}" class="gallery-image"></li>`;
 });
 
 // Вставляємо готовий HTML-код галереї в ul.gallery
-gallery.insertAdjacentHTML('beforeend', galleryHTML);
-
+gallery.insertAdjacentHTML("beforeend", galleryHTML);
